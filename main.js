cc9e1efef2c9e7a960a0447d69984ba16787d2f0
Lê Minh Quân <marucube35@gmail.com>
2022-03-08 22:49:58 +0700

    feat: update full responsive responsive for footer fix header's icon in mobile fix header margin add pagination and header sort switch prevent anchor default add backup image

--- main.js
+++ main.js
@@ -175,7 +175,7 @@ for (let i = 0; i < numberOfProducts; i++) {
     newProduct.innerHTML = `
 <div class="col">
     <a class="home-product-item" href="#">
-        <div class="home-product-item__img" style="background-image: url(https://cdnb.artstation.com/p/assets/images/images/017/714/067/large/antoine-van-bergen-masterball-avb.jpg?1557075802&dl=1);"></div>
+    <div class="home-product-item__img" style="background-image: url(https://cdnb.artstation.com/p/assets/images/images/017/714/067/large/antoine-van-bergen-masterball-avb.jpg?1557075802&dl=1), url(https://thumbs.dreamstime.com/b/placeholder-rgb-color-icon-image-gallery-photo-thumbnail-available-album-digital-media-multimedia-file-visual-content-snapshot-187369540.jpg);"></div>
         <h4 class="home-product-item__name">The Master Ball (Japanese: マスターボール Master Ball) is a type of Poké Ball introduced in Generation I. It can be used to catch a wild Pokémon without fail</h4>
         <div class="home-product-item__price">
             <span class="home-product-item__price-old">1.200.000đ</span>
@@ -236,9 +236,42 @@ for (const section of likeSections) {
 }
 
 
-/// Pagination buttom
+// Pagination button
 
-let dotsButton = document.querySelector(".pagination__item--disabled .pagination__link");
-dotsButton.addEventListener("click", function (event) {
-    event.preventDefault();
-})
+let pagiButtons = document.querySelectorAll(".pagination__item:not(.pagination__item--disabled)");
+let pagiTrigger = "pagination__item--active"
+let activePagiButton = document.querySelector(`.${pagiTrigger}`);
+
+for (let i = 1; i < pagiButtons.length - 1; i++) {
+    const button = pagiButtons[i]
+    button.addEventListener("click", function (event) {
+        event.preventDefault();
+        activePagiButton.classList.remove(pagiTrigger);
+        button.classList.add(pagiTrigger);
+        activePagiButton = button;
+    })
+}
+
+// Header Sort 
+
+let headerSortButtons = document.querySelectorAll(".header__sort-item")
+let headerSortTrigger = "header__sort-item--active"
+let activeHeaderSortButton = document.querySelector(`.${headerSortTrigger}`)
+
+for (const button of headerSortButtons) {
+    button.addEventListener("click", function(){
+        activeHeaderSortButton.classList.remove(headerSortTrigger)
+        button.classList.add(headerSortTrigger)
+        activeHeaderSortButton = button;
+    })
+    
+}
+
+// Disable anchor default
+
+let anchorTags = document.querySelectorAll('a');
+for (const tag of anchorTags) {
+    tag.addEventListener("click", function (e) {
+        e.preventDefault();
+    });
+}
